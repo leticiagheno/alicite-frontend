b19380cfdffec778bdffdbe3beb53f922ba843a7
Letícia <leeti@live.com>
2019-09-08 21:52:49 -0300

    :zap: Add routes to Home and About pages

--- alicite/src/router.js
+++ alicite/src/router.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
 import SignUp from "./pages/SignUp.vue";
+import Login from "./pages/Login.vue";
+import HomePage from "./pages/HomePage.vue";
+import About from "./pages/About.vue";
 
 Vue.use(Router);
 
@@ -8,8 +11,24 @@ export default new Router({
   routes: [
     {
       path: "/",
+      name: "homepage",
+      component: HomePage
+    },
+    {
+      path: "/login",
       name: "login",
+      component: Login
+    },
+    {
+      path: "/signup",
+      name: "signup",
       component: SignUp
-    }
+    },
+    {
+      path: "/sobre",
+      name: "about",
+      component: About
+    },
+
   ]
 });
