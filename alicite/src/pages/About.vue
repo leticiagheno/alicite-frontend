16473088e447cb2dea9b41f5a9f556b528ec0237
Letícia <leeti@live.com>
2019-09-08 21:52:01 -0300

    :sparkles: Create an About Page

---
+++ alicite/src/pages/About.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-container fill-height> 
+        <v-card
+            max-width="344"
+            class="mx-auto"
+        >
+            <v-card-title >Sobre</v-card-title>
+            <v-card-text>
+                <p>Trabalho final da disciplina de Prática Profissional Integrada II do IFRS - Bento Gonçalves. </p>
+                <p> Desenvolvido por Letícia Gheno Baldissarelli. </p>
+                <a href="https://br.freepik.com/fotos-gratis/bobinas-de-fio-de-costura_4379523.htm#page=1&query=maquina+de+costura+rosa&position=6"> Crédito foto - página inicial </a>
+            </v-card-text>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+import Vuetify from 'vuetify/lib';
+
+export default {
+  name: 'Login',
+  vuetify: new Vuetify(),
+  data: () => ({ }),
+  methods: {
+  }
+};
+</script>
+
+<style scoped>
+
+
+</style>
